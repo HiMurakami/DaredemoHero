254eed5cb7aa0bfa985a3f75acd83f0de92c0aed
hmurakami <hmurakami@mikilab.doshisha.ac.jp>
2015-05-09 23:58:24 +0900

    temp record

---
+++ src/com/example/hackuapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.hackuapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+		startActivity(intent);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
